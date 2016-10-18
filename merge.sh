58f4e1bffb8ac4a5767613a0e40c890e6b81976e
Guillaume Nargeot <killy971@gmail.com>
2016-10-18 17:28:17 +0900

    Convert the holiday calendar file to be joinable

--- merge.sh
+++ merge.sh
@@ -16,20 +16,26 @@ cd ..
 cd step2
 for i in tokyo-*-hourly-*.csv; do
     cat $i | \
-	sed s'/^\(....\)\/\([0-9]\)\//\1\/0\2\//' | \
-	sed s'/^\(.......\)\/\([0-9]\) /\1\/0\2 /' | \
-	sed s'/^\([^ ]*\) \([0-9]\):/\1 0\2:/' > ../step3/$i
+        sed s'/^\(....\)\/\([0-9]\)\//\1\/0\2\//' | \
+        sed s'/^\(.......\)\/\([0-9]\) /\1\/0\2 /' | \
+        sed s'/^\([^ ]*\) \([0-9]\):/\1 0\2:/' > ../step3/$i
+done
+for i in tokyo-*-holiday.csv; do
+    cat $i | \
+        sed s'/,/\//' | sed s'/,/\//' | \
+        grep -v -e ,Sat, -e ,Sun, | \
+        cut -d, -f1,4 | sed s'/,[MTWTF].*/,1/' > ../step3/$i
 done
 cd ..
 
 # join files
 cd step3
 for i in tokyo-*-hourly-temperature.csv; do
-    # 
-    # 
     YEAR=$(echo $i | grep -o "\b[0-9]\{4\}\b")
     join -t, --nocheck-order tokyo-$YEAR-hourly-temperature.csv tokyo-$YEAR-hourly-rainfall.csv | \
         join -t, --nocheck-order tokyo-$YEAR-hourly-illumination.csv - | \
-	sed s'///g' > ../step4/tokyo-$YEAR-hourly-stats.csv
+        tr ' ' ',' | \ # TODO add comma to split first field of first line and leading comma to second line
+        sed s'/
+//g' > ../step4/tokyo-$YEAR-hourly-stats.csv
 done
 cd ..
